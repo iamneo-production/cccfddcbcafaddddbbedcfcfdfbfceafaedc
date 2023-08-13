90550e8958310d57761ea711f3d2051390889b40
Iamneo <techadmin@iamneo.ai>
2023-08-13 08:26:01 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/dao/Taskdao.java
+++ springapp/src/main/java/com/examly/springapp/dao/Taskdao.java
@@ -1,3 +1,9 @@
-public class Taskdao {
+package com.examly.springapp.dao;
+
+import com.examly.springapp.entity.Task;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Taskdao extends JpaRepository<Task,Long> {
     
 }
